9050270d9bedd2d847bb7b87d551ed08c127adf9
Bjorn_Tore <Bjonne696@gmail.com>
2022-12-25 18:47:43 +0100

    update

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'f0c9a90ca6msh556070b3a5d1b4dp189e17jsn52d0a34e3e29',
+		'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+	}
+};
+
+fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=bbq', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
